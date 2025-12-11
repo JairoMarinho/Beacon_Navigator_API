c4be1fbcb9677687a4f1aae6ca2de408cef1a72d
Vitor <vitorvitorsantos2015@hotmail.com>
2025-12-11 11:37:06 -0300

    primeiro commit - BEACON NAVIGATOR

---
+++ src/main/java/com/beaconnavigator/api/BeaconNavigatorApplication.java
@@ -0,0 +1,13 @@
+package com.beaconnavigator.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeaconNavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeaconNavigatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beaconnavigator/api/BeaconNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beaconnavigator.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeaconNavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
