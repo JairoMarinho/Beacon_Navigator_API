4c77b2e299595347156f508502939a5bb608f632
Vitor <vitorvitorsantos2015@hotmail.com>
2025-12-12 13:21:04 -0300

    entidade atualizada

--- src/main/java/com/beaconnavigator/api/models/UsuarioBeacon.java
+++ src/main/java/com/beaconnavigator/api/models/UsuarioBeacon.java
@@ -1,5 +1,41 @@
 package com.beaconnavigator.api.models;
 
+import com.beaconnavigator.api.constants.StatusBeacon;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_usuario_beacon")
 public class UsuarioBeacon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // RELACIONAMENTO
+    @ManyToOne
+    @JoinColumn(name = "USUARIO_ID", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "BEACON_ID", nullable = false)
+    private Beacons beacon;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "STATUS_BEACON")
+    private StatusBeacon statusBeacon;
 
 }
