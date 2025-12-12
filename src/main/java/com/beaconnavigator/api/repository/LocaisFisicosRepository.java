8e427c40cbfb4f5c7872c80a8379138b21721502
Vitor <vitorvitorsantos2015@hotmail.com>
2025-12-12 15:21:58 -0300

    adicionada anotação adicionada

--- src/main/java/com/beaconnavigator/api/repository/LocaisFisicosRepository.java
+++ src/main/java/com/beaconnavigator/api/repository/LocaisFisicosRepository.java
@@ -1,6 +1,7 @@
 package com.beaconnavigator.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.beaconnavigator.api.models.LocaisFisicos;
 
