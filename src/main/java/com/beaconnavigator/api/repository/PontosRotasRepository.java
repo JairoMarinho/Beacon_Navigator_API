4f0ac25a3f00cf56c2466cd6c511cb572593bbf9
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-12-15 16:27:44 -0300

    Pontos Rotas repostirory modificado

--- src/main/java/com/beaconnavigator/api/repository/PontosRotasRepository.java
+++ src/main/java/com/beaconnavigator/api/repository/PontosRotasRepository.java
@@ -1,5 +1,7 @@
 package com.beaconnavigator.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.beaconnavigator.api.models.PontosRotas;
 
 @Repository
 public interface PontosRotasRepository extends JpaRepository<PontosRotas, Long> {
-
+    List<PontosRotas> findByRotaIdOrderBySequenceNumberAsc(Long rotaId);
 }
