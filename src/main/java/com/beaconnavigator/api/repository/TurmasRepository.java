8f84c3d11e51a1153361891278bccbf4413616b2
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-12-15 16:11:09 -0300

    turmas respository atualizado

--- src/main/java/com/beaconnavigator/api/repository/TurmasRepository.java
+++ src/main/java/com/beaconnavigator/api/repository/TurmasRepository.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import com.beaconnavigator.api.models.Turmas;
 
+import java.util.List;
+
 @Repository
 public interface TurmasRepository extends JpaRepository<Turmas, Long> {
-
+    List<Turmas> findByLocalFisicoId(Long localId);
 }
