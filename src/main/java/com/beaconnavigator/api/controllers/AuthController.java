091c6db24a9664a92d14f7f7c400349719e75bdd
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-12-16 20:18:13 -0300

    Auth Controller criado para criar end point de autenticação

---
+++ src/main/java/com/beaconnavigator/api/controllers/AuthController.java
@@ -0,0 +1,26 @@
+package com.beaconnavigator.api.controllers;
+
+import com.beaconnavigator.api.dtos.AuthLoginRequest;
+import com.beaconnavigator.api.dtos.AuthLoginResponse;
+import com.beaconnavigator.api.services.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthLoginResponse> login(@Valid @RequestBody AuthLoginRequest req) {
+        String token = authService.login(req);
+        return ResponseEntity.ok(new AuthLoginResponse(token));
+    }
+}
