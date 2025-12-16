25665c0121a50ea0904cda036613514783801beb
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-12-15 23:17:36 -0300

    adiciona métodos básicos de acesso a dados

--- src/main/java/com/beaconnavigator/api/repository/TurmasMatriculasRepository.java
+++ src/main/java/com/beaconnavigator/api/repository/TurmasMatriculasRepository.java
@@ -1,5 +1,7 @@
 package com.beaconnavigator.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,14 @@ import com.beaconnavigator.api.models.TurmasMatriculas;
 
 @Repository
 public interface TurmasMatriculasRepository extends JpaRepository<TurmasMatriculas, Long> {
+
+  // Para listar todos os alunos de uma turma específica
+  List<TurmasMatriculas> findByTurmaId(Long turmaId);
+
+  // Para listar todas as turmas de um aluno específico
+  List<TurmasMatriculas> findByUsuarioId(Long usuarioId);
+
+  // Para evitar duplicidade (Aluno já está na turma?)
+  boolean existsByTurmaIdAndUsuarioId(Long turmaId, Long usuarioId);
 
 }
