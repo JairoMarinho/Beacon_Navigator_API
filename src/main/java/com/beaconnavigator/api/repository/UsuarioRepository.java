f817cda776710a43c85ea1ccf205e3a934d5219b
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2025-12-16 20:22:16 -0300

    usuário repository atualizado

--- src/main/java/com/beaconnavigator/api/repository/UsuarioRepository.java
+++ src/main/java/com/beaconnavigator/api/repository/UsuarioRepository.java
@@ -1,14 +1,15 @@
 package com.beaconnavigator.api.repository;
 
-import java.util.Optional;
-
+import com.beaconnavigator.api.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.beaconnavigator.api.models.Usuario;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
